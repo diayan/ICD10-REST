61ca8a59b87de168fa87e75502a1cf304c4bd2bc
diayan <diayans@outlook.com>
2020-09-24 12:33:37 +0000

    create role and user models

---
+++ src/main/java/com/diayan/ICD10RESTProject/entity/RoleName.java
@@ -0,0 +1,7 @@
+package com.diayan.ICD10RESTProject.entity;
+
+public enum RoleName {
+    ROLE_USER,
+    ROLE_PM,
+    ROLE_ADMIN
+}
--- src/main/java/com/diayan/ICD10RESTProject/entity/User.java
+++ src/main/java/com/diayan/ICD10RESTProject/entity/User.java
@@ -1,6 +1,9 @@
 package com.diayan.ICD10RESTProject.entity;
 
+import org.hibernate.annotations.NaturalId;
+
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "user")
@@ -9,8 +12,58 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Size(min = 3, max = 50)
+    private String name;
+
+    @Size(min = 3, max = 50)
     private String userName;
 
+    @NaturalId
+    @Size(max = 50)
+    private String email;
+
+    @Size(min = 6, max = 50)
     private String password;
-    
+
+    public User(Long id, String userName, String email, String password) {
+        this.id = id;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
